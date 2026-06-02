14f6d0ba3746f532ca25817ea0fb318c3dc9c7bc
yuvaraj <yuvarajnagari2@gmail.com>
2026-06-02 16:34:11 +0530

    Updated provider configuration

--- provider.tf
+++ provider.tf
@@ -1,10 +1,14 @@
-terraform { 
-    required_providers {
-        aws = {
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
-} 
+  }
 
- provider "aws" {
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
   region = "ap-northeast-1"
 }
